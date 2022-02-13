8877847aef8c416c9ff297b51ec585bc9888b5a6
Cash <master_cash@live.com>
2022-02-13 07:47:18 -0600

    now using win api to do input

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import asyncio
+from math import remainder
 from bot import handleState, start
 
 from time import sleep, time
 from datamanager import DataManager
 from discordbot import DiscordRunner
-from windowcapture import CaptureData, getWindowInfo
+from windowManager import CaptureData, getWindowInfo
+def splitLocations(num: int, max: tuple[int, int] = (600, 600)) -> list[tuple[int, int, int, int]]:
+  chunk = int(max[0] / num)
+  extra = ((num * chunk) % max[0]) + 1
+  locs = [(chunk * i, 0, (chunk * (i+1)) -1, max[1]) for i in range(num)]
+  x1, y1, x2, y2 = locs[num -1]
+  locs[num -1] = (x1, y1, x2 + extra, y2)
+  return locs
 
 def main():
 
   database = DataManager()
   discord = DiscordRunner(database)
-  locs: list[tuple[int, int, int, int]] = [
-    (0, 0, 399, 399),
-    (400, 0, 600, 399),
-    (0, 400, 399, 600),
-    (400, 400, 600, 600)
-  ]
-
   captures: list[CaptureData] = []
   # add list of window names
-  for window in ["BlueStacks 2", ]:
-    captures.append(getWindowInfo(window))
+  for window in ["BlueStacks", "BlueStacks 1","BlueStacks 2", "BlueStacks 3"]:
+    info = getWindowInfo(window, "Qt5154QWindowOwnDCIcon")
+    # ignore windows we don't find
+    if info != None:
+      captures.append(info)
+
   numCaptures = len(captures)
-  stopped = list(map(lambda x: False, range(numCaptures)))
+  locs = splitLocations(numCaptures)
+  stopped = [False for _ in range(numCaptures)]
   killSwitch = [False]
+
   runTime = time()
   for i in range(numCaptures):
     start(handleState(captures[i], locs[i],database.addData, str(i)), i, stopped, killSwitch)
